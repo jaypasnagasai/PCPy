d44b770619c87b599f3929f8b15887e987981e02
jaypasnagasai <jay662466@gmail.com>
2025-06-26 17:59:33 -0700

    Change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PreCliPy',
-    version='0.1',
+    version='0.1.1',
     description='A biomedical lookup tool for diseases, drugs, PMC articles, and NCT trials',
     packages=find_packages(),
     install_requires=[
