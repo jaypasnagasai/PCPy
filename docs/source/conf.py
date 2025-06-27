46a814237b7b79dee6440d7367ea5bf3f190d201
jaypasnagasai <jay662466@gmail.com>
2025-06-27 00:40:53 -0700

    change conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../preclipy'))  # <- adjust if package path differs
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'PreCliPy'
 copyright = '2025, Jayanth Pasupulati'
@@ -12,14 +12,28 @@ author = 'Jayanth Pasupulati'
 release = '0.1.7'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',     # for Google/NumPy-style docstrings
+    'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+# -- Options for HTML output -------------------------------------------------
 
+html_theme = 'sphinx_rtd_theme'  # ← cleaner and more readable
+html_static_path = ['_static']
+
+# -- Autodoc options ---------------------------------------------------------
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
